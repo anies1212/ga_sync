cc3742ca652adf15bf6d19ffab702f8fbac2bf9b
anies1212 <all.for.me1212sna@gmail.com>
2026-02-04 21:01:32 +0900

    fix: dart format適用

--- lib/src/models/event_definition.dart
+++ lib/src/models/event_definition.dart
@@ -17,8 +17,9 @@ class EventDefinition {
     return eventName
         .split('_')
         .map(
-          (word) =>
-              word.isEmpty ? '' : '${word[0].toUpperCase()}${word.substring(1)}',
+          (word) => word.isEmpty
+              ? ''
+              : '${word[0].toUpperCase()}${word.substring(1)}',
         )
         .join('');
   }
